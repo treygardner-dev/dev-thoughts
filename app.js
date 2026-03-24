eb0a2ca91f974188a48c93fe15464e0bad8460d7
treygardner-dev <treygardner-dev@protonmail.com>
2026-03-24 14:47:13 -0600

    first official commit?

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+name: Trey Gardner
+project: dev-thoughts
+description:
+    This project is how I will hone my ability to create and deploy a website that is created 
+    entirely in raw html, css, and javascript. This website will allow me to post about anything
+    I feel like sharing with the world.
+*/
+
